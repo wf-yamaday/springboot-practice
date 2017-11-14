9c88af0fab97fb6135510fffbece2f0d2f068259
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2017-11-14 16:46:53 +0900

    Update RegistUser & Login

---
+++ src/main/java/com/example/service/LoginUserDetails.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import com.example.domain.User;
+import lombok.Data;
+
+@Data
+public class LoginUserDetails extends org.springframework.security.core.userdetails.User{
+	private final User user;
+	
+	public LoginUserDetails(User user){
+		super(user.getMail(), user.getPassword(), AuthorityUtils.createAuthorityList("ROLE_USER"));
+		this.user = user;
+	}
+}
